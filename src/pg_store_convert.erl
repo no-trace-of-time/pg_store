cc1b37bb9a292168f21dfa3a7b3a652cc99dda55
simonxu72 <simon.xu72@gmail.com>
2017-11-03 12:11:17 +0800

    convert test ok,add test routine,

--- src/pg_store_convert.erl
+++ src/pg_store_convert.erl
@@ -12,18 +12,36 @@
 %% API
 -export([
   convert_node_name/4
+  , t/0
+  , t_prod/0
 ]).
 
 
+t() ->
+  convert_node_name('zr_online_pg_sh@trust-test', 'nonode@nohost',
+    <<"mnesia.backup">>, <<"mnesia.restore">>).
+t_prod() ->
+  convert_node_name('zr_online_pg@trust-prod', 'nonode@nohost',
+    <<"mnesia.prod.backup">>, <<"mnesia.prod.restore">>).
+
+restore(RestoreFileName) ->
+  mnesia:install_fallback(RestoreFileName),
+  mnesia:stop(),
+  mnesia:start(),
+  lager:error("please waiting for data restore...", []),
+  mnesia:wait_for_tables([history_up_txn_log], infinity),
+  ok.
 %%------------------------------------------------------------------
 convert_node_name(From, To, FromFile, ToFile)
   when is_atom(From), is_atom(To), is_binary(FromFile), is_binary(ToFile) ->
 
   BackupDir = pg_store:backup_dir(),
+  FullFromFile = binary_to_list(<<BackupDir/binary, FromFile/binary>>),
+  FullToFile = binary_to_list(<<BackupDir/binary, ToFile/binary>>),
+  lager:error("FromFile = ~ts,ToFile = ~ts", [FullFromFile, FullToFile]),
+  do_convert_node_name(mnesia_backup, From, To, FullFromFile, FullToFile),
 
-  do_convert_node_name(mnesia_backup, From, To,
-    <<BackupDir/binary, FromFile/binary>>,
-    <<BackupDir/binary, ToFile/binary>>),
+  restore(FullToFile),
   ok.
 
 do_convert_node_name(Mod, From, To, Source, Target) ->
